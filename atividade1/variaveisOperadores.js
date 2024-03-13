6f387be38676ec5f4bccf658c52331191db418dc
filipe <130841497+heavwn@users.noreply.github.com>
2024-03-13 10:06:53 -0300

    Add files via upload

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,19 @@
+let n = 10;
+let b = 10;
+
+
+let op="+";
+let rslt = n + b;
+console.log(`o resultado da ${op} entre ${n} e ${b} é: ${rslt}`);
+
+op="-";
+rslt = n - b;
+console.log(`o resultado da ${op} entre ${n} e ${b} é: ${rslt}`);
+
+op="*";
+rslt = n * b;
+console.log(`o resultado da ${op} entre ${n} e ${b} é: ${rslt}`);
+
+op="/";
+ rslt = n / b;
+console.log(`o resultado da ${op} entre ${n} e ${b} é: ${rslt}`);
